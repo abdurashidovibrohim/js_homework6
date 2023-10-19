24a48bdf355900844976d3de32712fe328de0295
Your Name~ <you@example.com>
2023-10-19 12:56:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//1-Misol
+// function Names(...names){
+//      let arr1 = ["Abubakir","Umar","Usmon","Ali"]
+//      for (i = 0; i < arr1.length; i++){
+//          if( arr1 [i] == names ){
+//              console.log(`Array ichda ${names} bor`);   
+//             }
+//         }console.log(`Array ichida ${names} yo'q`)
+
+//     }Names("Doni") 
+
+// 2-Misolimage.png
+
+// numbers = [34, 56, 12, 23, 90]
+//  let larger = numbers[0];
+//  let smaller = numbers[0];
+//  for (let i = 1; i < numbers.length; i++) {
+//    if (numbers[i] > larger) {
+//      larger = numbers[i];
+//    } else if (numbers[i] < smaller) {     smaller = numbers[i];
+//    }}
+//    console.log(larger);
+//    console.log(smaller);
